27553841356ec2a327dad2fb96835af76e34bf2e
Martin Gonzalez <tinchogon34@gmail.com>
2012-09-17 19:27:42 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codingways::Application.config.secret_token = '2664e0479ad8f4f7075dae753addd5ed40d1da84e5ea61d01e313e7d5567ba3c0899d6d392b1912b5a8b4068c9b2e00d906bdaf635f575df027501afb416aa3f'
